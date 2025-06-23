c91e7709358c8030a22aa0a5053839f64525cf07
Sergey Sokolov <ssokolov85@gmail.com>
2025-06-23 16:13:06 +0300

    Удалил лишний test.py файл

--- src/python2025/test.py
+++
@@ -1,7 +0,0 @@
-SOME_LONG_STRING = "1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_123456789"
-
-
-d = {
-    "ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
-    {"dddddddddddddddddddddddddddddddddddddddddddddd"},
-}
